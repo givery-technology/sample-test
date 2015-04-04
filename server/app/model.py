aa5d9130d4379a0ba30b923d7a66111cbf02fa8c
Ixai Lanzagorta <ixai@ooyala.com>
2015-04-04 11:36:31 -0600

    Add Attend db model

--- server/app/model.py
+++ server/app/model.py
@@ -8,6 +8,7 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True)
     group_id = db.Column(db.Integer)
     events = db.relationship('Event')
+    attends = db.relationship('Attend')
 
     def __init__(self, name, password, email, group_id):
         self.name = name
@@ -18,6 +19,7 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.name
 
+
 class Event(db.Model):
     __tablename__ = 'events'
     id = db.Column(db.Integer, primary_key=True)
@@ -25,6 +27,7 @@ class Event(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     name = db.Column(db.String(100), unique=True)
     start_date = db.Column(db.DateTime)
+    attends = db.relationship('Attend')
 
     def __init__(self, user, name, start_date):
         self.user = user
@@ -33,3 +36,19 @@ class Event(db.Model):
 
     def __repr__(self):
         return '<Event %r>' % self.name
+
+
+class Attend(db.Model):
+    __tablename__ = 'attends'
+    user = db.relationship('User')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    event = db.relationship('Event')
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'), primary_key=True)
+    reserved_at = db.Column(db.DateTime)
+
+    def __init__(self, user, event):
+        self.user = user
+        self.event = event
+
+    def __repr__(self):
+        return '<Attend %r>' % self.event.name
