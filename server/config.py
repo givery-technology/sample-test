a38ca705088c2b590c2b0ab54f6adbb0e210c510
Ixai Lanzagorta <ixai@ooyala.com>
2015-04-04 18:57:42 -0600

    Add extra config options

--- server/config.py
+++ server/config.py
@@ -1,3 +1,9 @@
 class Config(object):
+    # SQL connection URI
     SQLALCHEMY_DATABASE_URI = 'mysql://cody:cody@localhost/cody'
+    # Listening port
+    PORT = 8888
+    # Reload the application when the source code is modified
+    USE_RELOADER = True
+    # Print debug information
     DEBUG = True
--- server/server.py
+++ server/server.py
@@ -1,2 +1,4 @@
 from app import app
-app.run(debug=True, use_reloader=True, port=8888)
+use_reloader = app.config['USE_RELOADER']
+port = app.config['PORT']
+app.run(use_reloader=use_reloader, port=port)
